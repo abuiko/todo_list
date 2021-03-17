b16c267f873d4018936c71959f2ba539f2c3d172
Alena Buiko <abuiko.ma@gmail.com>
2021-03-17 16:44:50 -0400

    select variables

---
+++ app.js
@@ -0,0 +1,16 @@
+// global variables
+const alert = document.querySelector('.alert');
+const list = document.querySelector('.grocery-list');
+
+// inputs 
+const groceryInput = document.getElementById('grocery');
+const groceryTitle = document.querySelector('.title');
+
+// containers
+const formContainer = document.querySelector('.form-container');
+const groceryContainer = document.querySelector('.grocery-container');
+
+// selected buttons
+const submitBtn = document.querySelector('.form-btn');
+const deleteBtn = document.querySelector('.delete-btn');
+const clearBtn = document.querySelector('.celar-btn');
